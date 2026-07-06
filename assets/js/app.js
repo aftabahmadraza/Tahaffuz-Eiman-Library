f8dceb6baf310a47a1450ed5bec1019b53f25eab
aftabahmadraza <aftabpsb@gmail.com>
2026-07-07 04:07:06 +0530

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/* =======================================
+Tahaffuz-E-Iman Library
+Main App
+======================================= */
+
+alert("APP JS Loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Tahaffuz-E-Iman Library Loaded");
+
+    initCounters();
+
+    loadLatestQuestions();
+
+});
